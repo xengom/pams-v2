972db72aa2f36ff4b6f03fa5176e72948a0d783e
xengom <xengom@gmail.com>
2025-09-12 17:33:01 +0900

    fix:web배포 용량오류 수정

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,11 @@
 ;;/** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@pams/types'],
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   async rewrites() {
     return [
       {
